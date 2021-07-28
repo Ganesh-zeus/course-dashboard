0d42115eb8dc07b2289694706fb50f27a4bb7533
Ganesh-zeus <ganesh.moota@zeuslearning.com>
2021-07-28 13:40:01 +0530

    added toggle functionality to links and nav-toggler

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,17 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, RouterEvent } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
-  activeLink = "DASHBOARD";
-  navLinkList = ["DASHBOARD","CONTENT","USERS","REPORTS","ADMIN"];
+  activeLink = 'DASHBOARD';
+  navLinkList = ['DASHBOARD', 'CONTENT', 'USERS', 'REPORTS', 'ADMIN'];
+  isDashboard: any;
 
-  constructor() { }
+  isMenuOpen:boolean = false;
+  isAlertsOpen:boolean = false;
+  isAnnouncementsOpen:boolean = false;
 
-  ngOnInit(): void {
+  constructor(private router: Router) {
+    router.events.subscribe((url: any) => {
+      this.isDashboard = router.routerState.snapshot.url === '/dashboard';
+    });
   }
 
+  ngOnInit(): void {}
 }
