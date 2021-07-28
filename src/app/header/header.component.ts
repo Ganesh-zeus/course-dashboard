60f25cbe27305c21b0bad048c83fc23cc8fcbd33
Ganesh-zeus <ganesh.moota@zeuslearning.com>
2021-07-28 11:07:13 +0530

    modified header component active links

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  activeLink = "DASHBOARD";
+  navLinkList = ["DASHBOARD","CONTENT","USERS","REPORTS","ADMIN"];
 
   constructor() { }
 
